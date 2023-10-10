b5fe9f61f228fc3545036a530ba0f54c3b9d5c0c
sigged <sderdeyn@gmail.com>
2023-10-10 11:35:29 +0200

    Add project files.

---
+++ Mde.Echo/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mde.Echo
+{
+    public partial class MainPage : ContentPage
+    {
+        private const int translationDelta = 600;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            txtInput.TranslationX = translationDelta;
+            btnEcho.TranslationX = txtInput.TranslationX;
+
+            txtInput.TranslateTo(
+                txtInput.X, txtInput.Y,
+                750,
+                Easing.BounceOut
+            );
+
+            btnEcho.TranslateTo(
+                btnEcho.X, btnEcho.Y,
+                750,
+                Easing.SpringOut
+            );
+        }
+
+    }
+}
